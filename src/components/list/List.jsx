01a90e8738415c9567c24e174ae3961b4f37cfd7
rdeswal <64295338+rdeswal@users.noreply.github.com>
2024-05-18 23:11:45 +0530

    two users can hat at same time

---
+++ src/components/list/List.jsx
@@ -0,0 +1,14 @@
+import ChatList from "./chatList/Chatlist";
+import "./list.css";
+import Userinfo from "./userInfo/Userinfo";
+
+const List = () => {
+  return (
+       <div className="list">
+         <Userinfo />
+         <ChatList />
+    </div>
+  )
+}
+
+export default List;
